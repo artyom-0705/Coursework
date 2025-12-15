fb3f08edac94b2c982c405520805203dcc974157
BtVar <parnisha.fin@bk.ru>
2025-12-15 17:43:18 +0700

    init files added

---
+++ Core/Inc/init.h
@@ -0,0 +1,2 @@
+#include ".\CMSIS\Devices\STM32F4xx\Inc\STM32F411xE\stm32f411xe.h"
+#include ".\CMSIS\Devices\STM32F4xx\Inc\stm32f4xx.h"
---
+++ Core/Src/init.c
@@ -0,0 +1,69 @@
+#include ".\Core\Inc\init.h"
+
+void SysTick_Init(void)
+{
+    CLEAR_BIT(SysTick->CTRL, SysTick_CTRL_ENABLE_Msk); // выключим счетчик
+    SET_BIT(SysTick->CTRL, SysTick_CTRL_TICKINT_Msk); // Разрешаем прерывание по системному таймеру
+    SET_BIT(SysTick->CTRL, SysTick_CTRL_CLKSOURCE_Msk); // источник тактирования AHB без делителя
+    MODIFY_REG(SysTick->LOAD, SysTick_LOAD_RELOAD_Msk, 95999 << SysTick_LOAD_RELOAD_Pos); // Значение с которого начинается счёт, эквивалентное 1 кГц 
+    MODIFY_REG(SysTick->VAL, SysTick_VAL_CURRENT_Msk, 95999 << SysTick_VAL_CURRENT_Pos);  // Очистка поля
+    SET_BIT(SysTick->CTRL, SysTick_CTRL_ENABLE_Msk);                                      // Включим счетчик
+}
+
+void ITR_Init(void)
+{
+    SET_BIT(RCC->APB2ENR, RCC_APB2ENR_SYSCFGEN); //Включение тактирования периферии SYSCFG 
+
+    MODIFY_REG(SYSCFG->EXTICR[3], SYSCFG_EXTICR4_EXTI12_Msk, 
+    SYSCFG_EXTICR4_EXTI12_PC); //Настройка мультиплексора на вывод линии прерывания EXTI13 на PC13 
+
+    // настройка EXTI регистров
+    SET_BIT(EXTI->IMR, EXTI_IMR_MR12); //Настройка маскирования 13 линии 
+    SET_BIT(EXTI->RTSR, EXTI_RTSR_TR12); //Настройка детектирования нарастающего фронта 13 линии 
+    SET_BIT(EXTI->FTSR, EXTI_FTSR_TR12); //Настройка детектирования спадающего фронта 13 линии 
+    NVIC_SetPriority(EXTI15_10_IRQn, 
+    NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0)); //Установка 0 приоритета прерывания для вектора EXTI15_10 
+    NVIC_EnableIRQ(EXTI15_10_IRQn); //Включение прерывания по вектору EXTI15_10 
+}
+
+void RCC_Init(void)
+{
+     /* Предварительная очистка регистров RCC */ 
+    MODIFY_REG(RCC->CR, RCC_CR_HSITRIM, 0x80UL); 
+    CLEAR_REG(RCC->CFGR); 
+    while(READ_BIT(RCC->CFGR, RCC_CFGR_SWS) != RESET); 
+    CLEAR_BIT(RCC->CR, RCC_CR_PLLON); 
+    while (READ_BIT(RCC->CR, RCC_CR_PLLRDY) != RESET); 
+    CLEAR_BIT(RCC->CR, RCC_CR_HSEON | RCC_CR_CSSON); 
+    while (READ_BIT(RCC->CR, RCC_CR_HSERDY) != RESET); 
+    CLEAR_BIT(RCC->CR, RCC_CR_HSEBYP); 
+ 
+    /* Настройка главного регистра RCC */ 
+    SET_BIT(RCC->CR, RCC_CR_HSION);                              
+    while (READ_BIT(RCC->CR, RCC_CR_HSIRDY) == RESET);
+ 
+    /*Настройка регистра PLLCFGR*/
+    CLEAR_REG(RCC->PLLCFGR);                                        // Очищаем регистр PLLCFGR
+    SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLSRC_HSI);                  // Источник тактирования HSI
+    SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLM_3);                      // Деление источника тактирования на 8
+    SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLN_6 | RCC_PLLCFGR_PLLN_5); // Настрока умножения на 96
+    CLEAR_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLP_0);                    // Делитель для выхода PLL(PLLCLK) на 2
+    SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLQ_2);                      // Делитель для USB на 15
+
+    /*Настройка регистра CFGR*/
+    SET_BIT(RCC->CFGR, RCC_CFGR_SW_PLL);                            // Источник системного тактирования -> выход PLL 96мгц
+    CLEAR_BIT(RCC->CFGR, RCC_CFGR_HPRE_DIV1);                       // Делитель AHB1 -> 1
+    SET_BIT(RCC->CFGR, RCC_CFGR_PPRE1_DIV2);                        // Делитель APB1 -> 2 (48 МГц)
+    SET_BIT(RCC->CFGR, RCC_CFGR_PPRE2_DIV1);                        // Делитель APB2 -> 1 (96 МГц) 
+    CLEAR_BIT(RCC->CFGR, RCC_CFGR_MCO2);  
+    CLEAR_BIT(RCC->CFGR, RCC_CFGR_MCO2_0 | RCC_CFGR_MCO2_1);        // Выход MCO2 -> Sysclk
+    SET_BIT(RCC->CFGR, RCC_CFGR_MCO2PRE_2 | RCC_CFGR_MCO2PRE_1);    // Делитель для MCO2 -> 4
+
+    /*Настрйока частоты FLASH-памяти*/
+    SET_BIT(FLASH->ACR, FLASH_ACR_LATENCY_3WS);
+
+    /*Включение блока PLL*/
+    SET_BIT(RCC->CR, RCC_CR_PLLON);
+    while (READ_BIT(RCC->CR, RCC_CR_PLLRDY) == RESET);
+
+}
