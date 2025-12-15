199841a117e95b9b9f0b376ae872bd8911aeaa75
BtVar <parnisha.fin@bk.ru>
2025-12-15 17:49:25 +0700

    it_handlers files added

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -1,2 +1,6 @@
 #include ".\CMSIS\Devices\STM32F4xx\Inc\STM32F411xE\stm32f411xe.h"
 #include ".\CMSIS\Devices\STM32F4xx\Inc\stm32f4xx.h"
+
+void ITR_Init(void);
+void SysTick_Init(void);
+void RCC_Init(void);
